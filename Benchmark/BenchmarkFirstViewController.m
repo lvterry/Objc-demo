c4e86e996132bdc2e55cfb0f478506877ebdaf60
Terry Wang <terry@alive.cn>
2014-05-07 22:22:59 +0800

    Make tableView searchable

--- Benchmark/BenchmarkFirstViewController.m
+++ Benchmark/BenchmarkFirstViewController.m
@@ -11,14 +11,14 @@
 @interface BenchmarkFirstViewController ()
 
 @property (nonatomic, strong) NSArray *albums;
+@property (nonatomic, strong) NSArray *searchResults;
 
 @end
 
 @implementation BenchmarkFirstViewController
 
 @synthesize albums = _albums;
-
-
+@synthesize searchResults = _searchResults;
 
 - (void)viewDidLoad
 {
@@ -41,25 +41,50 @@
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    return @" "; //To nudge the content down
+    return @"专辑"; //To nudge the content down
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return _albums.count;
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        return _searchResults.count;
+    } else {
+        return _albums.count;
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *CellIdentifier = @"Cell";
+    [tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
     // Configure the cell...
     NSDictionary *album = [_albums objectAtIndex:indexPath.row];
+    if (tableView == self.searchDisplayController.searchResultsTableView) {
+        album = [_searchResults objectAtIndex:indexPath.row];
+    }
     cell.textLabel.text = [album objectForKey:@"name"];
     cell.detailTextLabel.text = [album objectForKey:@"artist"];
     cell.imageView.image = [UIImage imageNamed:[album objectForKey:@"cover"]];
     
     return cell;
 
+}
+
+- (void)filterContentForSearchText:(NSString*)searchText scope:(NSString*)scope
+{
+    NSPredicate *resultPredicate = [NSPredicate predicateWithFormat:@"name contains[c] %@", searchText];
+    _searchResults = [_albums filteredArrayUsingPredicate:resultPredicate];
+    NSLog(@"%@", _searchResults);
+}
+
+-(BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
+{
+    [self filterContentForSearchText:searchString
+            scope:[[self.searchDisplayController.searchBar scopeButtonTitles]
+                objectAtIndex:[self.searchDisplayController.searchBar
+                selectedScopeButtonIndex]]];
+   
+    return YES;
 }
 
 @end
